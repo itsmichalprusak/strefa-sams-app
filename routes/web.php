6de032315aa8dff97ecb97f46c7f2a59cc4ef6c9
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-23 19:58:52 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,50 @@ Route::get('/', [
     'uses' => 'SitesController@index',
     'as' => 'home',
 ]);
+
 Route::get('/add', [
     'uses' => 'SitesController@add',
     'as' => 'add',
 ]);
+
+Route::post('/add', [
+    'uses' => 'SitesController@addemployee',
+    'as' => 'addemployee',
+]);
+
 Route::get('/insurance', [
     'uses' => 'SitesController@insurance',
     'as' => 'insurance',
 ]);
+
 Route::get('/search', [
     'uses' => 'SitesController@search',
     'as' => 'search',
 ]);
+
 Route::get('/base', [
     'uses' => 'SitesController@base',
     'as' => 'base',
 ]);
+
 Route::get('/home', [
     'uses' => 'SitesController@home',
     'as' => 'home',
+]);
+
+Route::get('/user', [
+    'uses' => 'SitesController@user',
+    'as' => 'user',
+]);
+
+Route::get('/patient', [
+    'uses' => 'SitesController@patient',
+    'as' => 'patient',
+]);
+
+Route::post('/patient', [
+    'uses' => 'SitesController@addpatient',
+    'as' => 'addpatient',
 ]);
 
 
