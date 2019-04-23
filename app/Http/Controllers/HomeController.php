e860ccdd66a3809f534670d80b8f565294ed5c50
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-23 19:55:26 +0200

    use Illuminate\Support\Facades\DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
