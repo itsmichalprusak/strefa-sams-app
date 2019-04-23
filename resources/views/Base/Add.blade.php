53fe5aedd200991de34cdbff9b3bd4dd3fc6d5cc
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-23 19:57:33 +0200

    add patient blade

--- resources/views/Base/Add.blade.php
+++ resources/views/Base/Add.blade.php
@@ -1,1 +1,36 @@
-<?php
+@extends('head')
+
+@section('title', 'SAMS - Dodaj Pracownika')
+
+@section('body')
+
+    <form method="POST" action="{{route('addemployee')}}">
+        {{csrf_field()}}
+
+        <div>
+            <label>Imie</label>
+            <input type="text" name="name" placeholder="Imie">
+        </div>
+        <div>
+            <label>Nazwisko</label>
+            <input type="text" name="surname" placeholder="Nazwisko">
+        </div>
+        <div>
+            <label>Ranga</label>
+            <input type="text" name="rank" placeholder="Ranga">
+        </div>
+        <div>
+            <label>Data urodzin</label>
+            <input type="date" name="date">
+        </div>
+        <div>
+            <label>Numer telefonu</label>
+            <input type="text" name="phonenumber" placeholder="Numer Telefonu">
+        </div>
+        <div>
+            <input type="submit" value="Dodaj Pracownika">
+        </div>
+
+    </form>
+
+@endsection
