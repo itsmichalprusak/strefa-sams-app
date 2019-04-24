49ed67dfadd6a1cfe3005ea74d8b4a062cf91cf7
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-24 15:18:44 +0200

    Update list.blade.php

--- resources/views/profiles/list.blade.php
+++ resources/views/profiles/list.blade.php
@@ -5,15 +5,18 @@
 @section('content')
 
     <div class="table-responsive">
-        <table class="table" width="100%">
+        <input type="text" id="Input" onkeyup="SearchByName()" placeholder="Szukaj po imieniu">
+        <table id="table" class="table" width="100%">
             <tr>
                 <th>Imię i Nazwisko</th>
+                <th>Ubezpieczony</th>
                 <th>Data urodzenia</th>
                 <th>Numer telefonu</th>
             </tr>
             @foreach($patients as $patient)
                 <tr>
                     <td><a href="{{route('user')}}?id={{$patient->Id}}">{{$patient->Name}} {{$patient->Surname}}</a></td>
+                    <td>@if($patient->IsInsured == 0) Nie @else Tak @endif</td>
                     <td>{{$patient->BirthDate}}</td>
                     <td>{{$patient->PhoneNumber}}</td>
                 </tr>
