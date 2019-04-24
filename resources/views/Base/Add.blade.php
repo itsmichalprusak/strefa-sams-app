1aa4658bd5b89b8c521a3577308eee303d292811
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-24 15:20:08 +0200

    Update Add.blade.php

--- resources/views/Base/Add.blade.php
+++ resources/views/Base/Add.blade.php
@@ -4,31 +4,44 @@
 
 @section('body')
 
+    <h1 style="margin-top: 10px;" >StrefaRP - Baza SAMS > Dodaj Pracownika</h1>
+
     <form method="POST" action="{{route('addemployee')}}">
         {{csrf_field()}}
 
-        <div>
-            <label>Imię</label>
-            <input type="text" name="name" placeholder="Imie">
+        <div class="form-group">
+            <label for="imie">Imię</label>
+            <input type="name" class="form-control" id="imie" aria-describedby="firstname" name="name" placeholder="Imię Lekarza">
+            <small id="firstname" class="form-text text-muted">Podaj imię pracownika, którego chcesz dodać do bazy.</small>
+        </div>
+        <div class="form-group">
+            <label for="nazwisko">Nazwisko</label>
+            <input type="name" name="surname" class="form-control" id="nazwisko" placeholder="Nazwisko Lekarza">
+            <small id="surname" class="form-text text-muted">Podaj nazwisko pracownika, którego chcesz dodać do bazy.</small>
+        </div>
+        <div class="form-group">
+            <label for="ranga">Ranga</label>
+            <select class="form-control" name="rank" id="stopien">
+                <option value="Szef SAMS">Szef SAMS</option>
+                <option value="Dyrektor">Dyrektor</option>
+                <option value="Lekarz ratownictwa medycznego">Lekarz ratownictwa medycznego</option>
+                <option value="Ratownik Medyczny">Ratownik Medyczny</option>
+                <option value="Szef SAMS">Pielęgniarz</option>
+            </select>
+            <small id="stopien" class="form-text text-muted">Podaj stopień pracownika, którego chcesz dodać do bazy.</small>
+        </div>
+        <div class="form-group">
+            <label for="dataurodzenia">Data urodzin</label>
+            <input type="date" name="date" class="form-control" id="dataurodzenia" onfocus="(this.type='date')">
+            <small id="surname" class="form-text text-muted">Podaj datę urodzenia pracownika, którego chcesz dodać do bazy.</small>
+        </div>
+        <div class="form-group">
+            <label for="nazwisko">Numer telefonu</label>
+            <input type="phone" name="phonenumber" class="form-control" id="telefon" placeholder="Numer Telefonu">
+            <small id="surname" class="form-text text-muted">Podaj numer telefonu pracownika, którego chcesz dodać do bazy.</small>
         </div>
         <div>
-            <label>Nazwisko</label>
-            <input type="text" name="surname" placeholder="Nazwisko">
-        </div>
-        <div>
-            <label>Ranga</label>
-            <input type="text" name="rank" placeholder="Ranga">
-        </div>
-        <div>
-            <label>Data urodzin</label>
-            <input type="date" name="date">
-        </div>
-        <div>
-            <label>Numer telefonu</label>
-            <input type="text" name="phonenumber" placeholder="Numer Telefonu">
-        </div>
-        <div>
-            <input type="submit" value="Dodaj Pracownika">
+            <button type="submit" class="btn btn-primary">Dodaj pracownika!</button>
         </div>
 
     </form>
