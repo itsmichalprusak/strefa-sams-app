f54a737c89e1154a52c0f5ff9293e16dba3916b9
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-24 15:17:09 +0200

    routes by name

--- resources/views/profiles/user.blade.php
+++ resources/views/profiles/user.blade.php
@@ -21,7 +21,7 @@
                     <tr>
                         <th>Imie i Nazwisko</th>
                         <th>Data Urodzenia</th>
-                        <th>Ubezpieczony?</th>
+                        <th>Ubezpieczony</th>
                         <th>Komentarze</th>
                         <th>Grupa Krwi</th>
                     </tr>
@@ -44,7 +44,7 @@
                             <th>Lekarz Nadzorujący</th>
                             <th>Kategoria zabiegu</th>
                             <th>Cena</th>
-                            <th>Zapłacono?</th>
+                            <th>Zapłacono</th>
                             <th>Rozpoznanie</th>
                             <th>Zabieg</th>
                         </tr>
@@ -53,8 +53,8 @@
                                 <td>{{$card->Name}} {{$card->Surname}}</td>
                                 <td>{{$card->Annotation}}</td>
                                 <td>{{$card->Date}}</td>
-                                <td><a href="/user?emId={{$card->emId}}">{{$card->emName}} {{$card->emSurname}}</a></td>
-                                <td>{{$card->TreatmentCategory}}</td>
+                                <td><a href="{{route('user')}}?emId={{$card->emId}}">{{$card->emName}} {{$card->emSurname}}</a></td>
+                                <td>{{$card->TreatmentCategory}} | {{$card->Description}}</td>
                                 <td>{{$card->Price}}</td>
                                 <td>@if($card->IsPaid == 0) Nie @else Tak @endif</td>
                                 <td>{{$card->Recognition}}</td>
@@ -80,10 +80,36 @@
                                     <td>{{$employee->Rank}}</td>
                                     <td>{{$employee->BirthDate}}</td>
                                     <td>{{$employee->PhoneNumber}}</td>
-                                    <td><a href="/user?emId={{$employee->UnderSupervision}}">{{$employee->UnderSupervision}}</a></td>
+                                    <td><a href="{{route('user')}}?emId={{$employee->UnderSupervision}}">{{$employee->UnderSupervision}}</a></td>
                                 </tr>
                             @endforeach
                         </table>
+                        <br><br><br>
+                        <table style="width: 100%">
+                            <tr>
+                                <th>Imie i Nazwisko</th>
+                                <th>Odesłanie</th>
+                                <th>Data</th>
+                                <th>Kategoria Zabiegu</th>
+                                <th>Cena</th>
+                                <th>Zapłacono</th>
+                                <th>Rozpoznanie</th>
+                                <th>Zabieg</th>
+                            </tr>
+                            @foreach ($cards as $card)
+                                <tr>
+                                    <td><a href="{{route('user')}}?id={{$card->Id}}">{{$card->Name}} {{$card->Surname}}</a></td>
+                                    <td>{{$card->Annotation}}</td>
+                                    <td>{{$card->Date}}</td>
+                                    <td>{{$card->TreatmentCategory}} | {{$card->Description}}</td>
+                                    <td>{{$card->Price}}</td>
+                                    <td>{{$card->IsPaid}}</td>
+                                    <td>{{$card->Recognition}}</td>
+                                    <td>{{$card->Treatment}}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+
                     @endif
                 </div>
             </div>
