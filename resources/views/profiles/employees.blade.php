4c167da0c4d83e7c7abdbd86ead8cc8016fa8786
dusgdwah <wikikikikiminecraft@gmail.com>
2019-04-24 15:19:19 +0200

    Create employees.blade.php

---
+++ resources/views/profiles/employees.blade.php
@@ -0,0 +1,27 @@
+@extends('head')
+
+@section('title', 'SAMS - Lista pracowników')
+
+@section('content')
+
+    <div class="table-responsive">
+        <input type="text" id="Input" onkeyup="SearchByName()" placeholder="Szukaj po imieniu">
+        <table id="table" class="table" width="100%">
+            <tr>
+                <th>Imię i Nazwisko</th>
+                <th>Ranga</th>
+                <th>Data urodzenia</th>
+                <th>Numer telefonu</th>
+            </tr>
+            @foreach($employees as $employee)
+                <tr>
+                    <td><a href="{{route('user')}}?emId={{$employee->Id}}">{{$employee->Name}} {{$employee->Surname}}</a></td>
+                    <td>{{$employee->Rank}}</td>
+                    <td>{{$employee->BirthDate}}</td>
+                    <td>{{$employee->PhoneNumber}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+
+@endsection
