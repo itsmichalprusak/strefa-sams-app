6076781638a3bc734be15a9dce83d74e5a1d0925
dusgdwah <allahaka90@gmail.com>
2019-04-25 10:40:04 +0200

    Update Patient.blade.php

--- resources/views/Base/Patient.blade.php
+++ resources/views/Base/Patient.blade.php
@@ -4,36 +4,43 @@
 
 @section('body')
 
+    <h3 style="margin-top: 10px;">Baza SAMS > Dodaj Pacjenta</h3>
     <form method="POST" action="{{route('addpatient')}}">
         {{csrf_field()}}
 
-        <div>
-            <label>Imię</label>
-            <input type="text" name="Name" placeholder="Imie">
+        <div class="form-group">
+            <label for="imie">Imię</label>
+            <input type="name" name="Name" id="imie" aria-describedby="firstname" placeholder="Imię Pacjenta" class="form-control">
+            <small id="firstname" class="form-text text-muted">Podaj imię pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div>
-            <label>Nazwisko</label>
-            <input type="text" name="Surname" placeholder="Nazwisko">
+            <label for="nazwisko">Nazwisko</label>
+            <input type="name" name="Surname" id="nazwisko" aria-describedby="nazwisko" placeholder="Nazwisko Pacjenta" class="form-control">
+            <small id="nazwisko" class="form-text text-muted">Podaj Nazwisko pacjenta, którego chcesz dodać do bazy.</small>
         </div>
-        <div>
-            <label>Email</label>
-            <input type="text" name="Email" placeholder="Email">
+        <div class="form-group">
+            <label for="Email">Nick Discord</label>
+            <input type="name" name="Email" id="Email" placeholder="Nick Discord" aria-describedby="Email" class="form-control">
+            <small id="Email" class="form-text text-muted">Podaj nick na Discord'dzie pacjenta, którego chcesz dodać do bazy.</small>
         </div>
-        <div>
-            <label>Numer telefonu</label>
-            <input type="text" name="PhoneNumber" placeholder="Numer Telefonu">
+        <div class="form-group">
+            <label for="PhoneNumber">Numer telefonu</label>
+            <input type="text" id="PhoneNumber" name="PhoneNumber" placeholder="Numer Telefonu" aria-describedby="PhoneNumber" class="form-control">
+            <small id="PhoneNumber" class="form-text text-muted">Podaj Numer Telefonu pacjenta, którego chcesz dodać do bazy.</small>
         </div>
-        <div>
-            <label>Data urodzin</label>
-            <input type="date" name="date">
+        <div class="form-group">
+            <label for="dataurodzenia">Data urodzin</label>
+            <input type="date" name="date" class="form-control" id="dataurodzenia" onfocus="(this.type='date')">
+            <small id="date" class="form-text text-muted">Podaj datę urodzenia pacjenta, którego chcesz dodać do bazy.</small>
         </div>
-        <div>
-            <label>Uwagi</label>
-            <textarea cols="50" name="Comments" placeholder="Choroby, Leki, Leczenia"></textarea>
+        <div class="form-group">
+            <label for="Comments">Uwagi</label>
+            <textarea name="Comments" class="form-control" id="Comments" aria-describedby="Comments" placeholder="Choroby, Leki, Leczenia"></textarea>
+            <small id="Comments" class="form-text text-muted">Podaj Alergie, leki itd. pacjenta, którego chcesz dodać do bazy.</small>
         </div>
-        <div>
-            <label>Grupa Krwi</label>
-            <select name="BloodGroup">
+        <div class="form-group">
+            <label for="BloodGroup">Grupa Krwi</label>
+            <select name="BloodGroup" class="form-control" id="BloodGroup" aria-describedby="BloodGroup">
                 <option value="1">0 Rh+</option>
                 <option value="2">0 Rh-</option>
                 <option value="3">A Rh+</option>
@@ -43,9 +50,10 @@
                 <option value="7">AB Rh+</option>
                 <option value="8">AB Rh-</option>
             </select>
+            <small id="BloodGroup" class="form-text text-muted">Podaj Grupę krwi pacjenta, którego chcesz dodać do bazy.</small>
         </div>
-        <div>
-            <input type="submit" value="Dodaj Pacjenta">
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary"> Dodaj Pacjenta!</button>
         </div>
 
     </form>
