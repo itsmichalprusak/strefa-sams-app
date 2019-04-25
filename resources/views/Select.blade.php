a531b1cef92e4a3b7f734098340f7ae3484165bc
dusgdwah <allahaka90@gmail.com>
2019-04-25 10:40:32 +0200

    Update Select.blade.php

--- resources/views/Select.blade.php
+++ resources/views/Select.blade.php
@@ -4,11 +4,11 @@
 
 @section('body')
     @if(Auth::guest())
-<div class="card text-center" style="margin-top: 10px;">
-<div class="card-body">
-<a href="{{route('login')}}" class="btn btn-primary btn-lg">Wejdź do bazy!</a>
-</div>
-</div>
+        <div class="card text-center" style="margin-top: 10px;">
+            <div class="card-body">
+                <a href="{{route('login')}}" class="btn btn-primary btn-lg">Wejdź do bazy!</a>
+            </div>
+        </div>
     @else
         <script>window.location = "{{route('home')}}";</script>
     @endif
