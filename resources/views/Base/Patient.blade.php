46091d44d223015101b83a9556a43d8c2a78c5da
dusgdwah <allahaka90@gmail.com>
2019-04-26 14:19:20 +0200

    Update Patient.blade.php

--- resources/views/Base/Patient.blade.php
+++ resources/views/Base/Patient.blade.php
@@ -10,37 +10,37 @@
 
         <div class="form-group">
             <label for="imie">Imię</label>
-            <input type="name" name="Name" id="imie" aria-describedby="firstname" placeholder="Imię Pacjenta" class="form-control">
+            <input required type="name" name="Name" id="imie" aria-describedby="firstname" placeholder="Imię Pacjenta" class="form-control bg-dark text-white">
             <small id="firstname" class="form-text text-muted">Podaj imię pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div class="form-group">
             <label for="nazwisko">Nazwisko</label>
-            <input type="name" name="Surname" id="nazwisko" aria-describedby="nazwisko" placeholder="Nazwisko Pacjenta" class="form-control">
+            <input required type="name" name="Surname" id="nazwisko" aria-describedby="nazwisko" placeholder="Nazwisko Pacjenta" class="form-control bg-dark text-white">
             <small id="nazwisko" class="form-text text-muted">Podaj Nazwisko pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div class="form-group">
             <label for="Email">Nick Discord</label>
-            <input type="name" name="Email" id="Email" placeholder="Nick Discord" aria-describedby="Email" class="form-control">
+            <input required type="name" name="Email" id="Email" placeholder="Nick Discord" aria-describedby="Email" class="form-control bg-dark text-white">
             <small id="Email" class="form-text text-muted">Podaj nick na Discord'dzie pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div class="form-group">
             <label for="PhoneNumber">Numer telefonu</label>
-            <input type="text" id="PhoneNumber" name="PhoneNumber" placeholder="Numer Telefonu" aria-describedby="PhoneNumber" class="form-control">
+            <input required type="text" id="PhoneNumber" name="PhoneNumber" placeholder="Numer Telefonu" aria-describedby="PhoneNumber" class="form-control bg-dark text-white">
             <small id="PhoneNumber" class="form-text text-muted">Podaj Numer Telefonu pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div class="form-group">
             <label for="dataurodzenia">Data urodzin</label>
-            <input type="date" name="date" class="form-control" id="dataurodzenia" onfocus="(this.type='date')">
+            <input type="date" name="date" class="form-control bg-dark text-white" id="dataurodzenia" onfocus="(this.type='date')">
             <small id="date" class="form-text text-muted">Podaj datę urodzenia pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div class="form-group">
             <label for="Comments">Uwagi</label>
-            <textarea name="Comments" class="form-control" id="Comments" aria-describedby="Comments" placeholder="Choroby, Leki, Leczenia"></textarea>
+            <textarea required name="Comments" class="form-control bg-dark text-white" id="Comments" aria-describedby="Comments" placeholder="Choroby, Leki, Leczenia"></textarea>
             <small id="Comments" class="form-text text-muted">Podaj Alergie, leki itd. pacjenta, którego chcesz dodać do bazy.</small>
         </div>
         <div class="form-group">
             <label for="BloodGroup">Grupa Krwi</label>
-            <select name="BloodGroup" class="form-control" id="BloodGroup" aria-describedby="BloodGroup">
+            <select required name="BloodGroup" class="form-control bg-dark text-white" id="BloodGroup" aria-describedby="BloodGroup">
                 <option value="1">0 Rh+</option>
                 <option value="2">0 Rh-</option>
                 <option value="3">A Rh+</option>
