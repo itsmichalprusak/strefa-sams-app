f48c240ec07aac2a3a1493da5a7ef354bd7bd40b
dusgdwah <allahaka90@gmail.com>
2019-04-27 18:49:02 +0200

    Create 2019_04_23_140511_create_foreign_keys_table.php

---
+++ database/migrations/2019_04_23_140511_create_foreign_keys_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForeignKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Patients', function (Blueprint $table) {
+            $table->foreign('InsuranceID')->references('Id')->on('Insurances');
+            $table->foreign('BloodGroupID')->references('Id')->on('BloodGroups');
+        });
+
+        //Schema::table('BloodGroups', function (Blueprint $table) {
+
+        //});
+
+        Schema::table('Insurances', function (Blueprint $table) {
+            $table->foreign('PatientID')->references('Id')->on('Patients');
+            $table->foreign('PersonIssuing')->references('Id')->on('Employees');
+        });
+
+        //Schema::table('Employees', function (Blueprint $table) {
+
+        //});
+
+        Schema::table('CardIndexes', function (Blueprint $table) {
+            $table->foreign('PatientId')->references('Id')->on('Patients');
+            $table->foreign('SupervisingDoctor')->references('Id')->on('Employees');
+            $table->foreign('TreatmentCategoryId')->references('Id')->on('Treatments');
+        });
+
+        //Schema::table('Treatments', function (Blueprint $table) {
+
+        //});
+
+        Schema::table('AuthorizedPersons', function (Blueprint $table) {
+            $table->foreign('PatientID')->references('Id')->on('Patients');
+            $table->foreign('AuthorizedPerson')->references('Id')->on('Patients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foreign_keys');
+    }
+}
